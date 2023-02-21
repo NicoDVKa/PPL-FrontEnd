df00e01ad5d0d6e5a306bf7924dec967085cccc1
NicoDVKa <nicodvkcss@gmail.com>
2023-02-21 13:14:38 -0300

    Integracion con Socket.io

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // http://localhost:3000  Pruebas
 // https://ppl-backend-production.up.railway.app Produccion
 
-export const urlBase = 'http://localhost:3000';
+export const urlBase = 'https://ppl-backend-production.up.railway.app';
